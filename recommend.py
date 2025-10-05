a682b7c64a1fc9331916d1a01291df301b782376
Nickeniklas <savonhen@arcada.fi>
2025-10-05 18:28:46 +0300

    hello world

---
+++ recommend.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import NMF
+
+print("Hello World!")
