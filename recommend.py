eb68e190988a98f80f419b5115df534de9bd725b
Nickeniklas <savonhen@arcada.fi>
2025-10-14 13:29:27 +0300

    ContentBasedRecommender()

--- recommend.py
+++ recommend.py
@@ -5,28 +5,46 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import NMF
 
 class ContentBasedRecommender:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, cars_data):
+        self.cars_data = cars_data
         self.tfidf_matrix = None
         self.cosine_sim = None
 
-    def preprocess(self):
-        self.data['features'] = self.data['features'].fillna('')
+    def fit(self):
+        self.cars_data['Features'] = self.cars_data['Features'].fillna('')
         tfidf = TfidfVectorizer(stop_words='english')
-        self.tfidf_matrix = tfidf.fit_transform(self.data['features'])
-
-    def compute_similarity(self):
-        self.cosine_sim = cosine_similarity(self.tfidf_matrix, self.tfidf_matrix)
-
-    def recommend(self, item_id, top_n=5):
-        idx = self.data.index[self.data['id'] == item_id][0]
-        sim_scores = list(enumerate(self.cosine_sim[idx]))
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-        sim_scores = sim_scores[1:top_n+1]
-        item_indices = [i[0] for i in sim_scores]
-        return self.data.iloc[item_indices]
+        self.tfidf_matrix = tfidf.fit_transform(self.cars_data['Features'])
+        self.car_indices = pd.Series(self.cars_data.index, index=self.cars_data['Make Model']).drop_duplicates()
+        #print("Fit:", self.car_indices)
+        #print(self.tfidf_matrix.shape) 
+    
+    def recommend(self, car, n=10):
+        if car not in self.car_indices: return []
+        print("Content based recommends for", car)
+        idx = int(self.car_indices[car].iloc[0])
+        sim_scores = cosine_similarity(self.tfidf_matrix[idx], self.tfidf_matrix).flatten()
+        sim_indices = sim_scores.argsort()[-n-1:-1][::-1]
+        return self.cars_data['Make Model'].iloc[sim_indices].tolist()
 
 class HybridRecommender:
-    def __init__(self, car_data_path):
-        self.car_data = pd.read_csv(car_data_path)
+    def __init__(self, cars_data_path, ratings_data_path):
+        self.car_data = pd.read_csv(cars_data_path)
+        self.rating_data = pd.read_csv(ratings_data_path)
         self.content_recommender = ContentBasedRecommender(self.car_data)
+        
+        
+if __name__ == "__main__":
+    # get datasets
+    cars_data_path = "./data/df_cars_clean.csv"
+    ratings_data_path = "./data/df_ratings_clean.csv"
+    cars_data = pd.read_csv(cars_data_path)
+    ratings_data = pd.read_csv(ratings_data_path)
+    #print("cars data: \n", cars_data)
+    #print("ratings data: \n", ratings_data)
+
+    # test content based recommender
+    recommender = ContentBasedRecommender(cars_data)
+    recommender.fit()
+
+    print(recommender.recommend('toyota sienna', n=5))
+    
